8d167c3365d38e78a8e318d0059c1fb2f470604f
Justin Bradford <jabradford@gmail.com>
2022-10-25 00:39:29 -0700

    Rough sketch of basic structure

---
+++ ring.ts
@@ -0,0 +1,31 @@
+type Optional<T> = T | undefined
+export type Entry<T> = { i: Optional<T>, n: Entry<T>, p: Entry<T> }
+
+export default class Ring<T> {
+  private root = {} as Entry<T>
+  private current : Entry<T>
+
+  constructor() {
+    this.current = this.root.n = this.root.p = this.root
+  }
+
+  add(item : T) : Entry<T> {
+    const before = this.current
+    const added = { i: item, n: before, p: before.p }
+    before.p = added.p.n = added
+    return before
+  }
+
+  remove(entry: Entry<T>) {
+    entry.p.n = entry.n
+    entry.n.p = entry.p
+    delete entry.i
+  }
+
+  next() : Optional<T> {
+    this.current = this.current.n
+    // when on the root entry, try skipping over it
+    if (this.current === this.root) this.current = this.current.n
+    return this.current.i
+  }
+}

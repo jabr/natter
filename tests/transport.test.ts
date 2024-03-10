839ed6a5948e354350d21f6a53258091000eed4c
Justin Bradford <jabradford@gmail.com>
2024-03-10 13:48:03 -0700

    Initial tests on transport interface and abstract classes

--- tests/transport.test.ts
+++ tests/transport.test.ts
@@ -1,1 +1,66 @@
-// @todo
+import {
+  describe, it, beforeEach,
+  expect, assert,
+} from './deps.ts'
+
+import {
+  Address, MessageType, Message,
+  Transport, ConfiguredTransport, QueuedTransport
+} from '../transport.ts'
+
+describe('ConfiguredTransport', () => {
+  let transport: ConfiguredTransport
+
+  beforeEach(() => {
+    class ConfiguredTransportImpl extends ConfiguredTransport {}
+    transport = new ConfiguredTransportImpl({
+      local: 'local',
+      roots: [ 'root1', 'root2', 'root3' ]
+    })
+  })
+
+  it('implements the Transport interface', () => {
+    // this will fail to compile if it doesn't implement the interface
+    const x: Transport = transport
+    expect(x).toBeDefined()
+  })
+
+  it('#local returns the configured address', () => {
+    expect(transport.local()).toBe('local')
+  })
+
+  it('#roots returns the configured address', () => {
+    const roots = transport.roots()
+    expect(roots.length).toBe(3)
+    expect(roots).toContain('root1')
+    expect(roots).toContain('root2')
+    expect(roots).toContain('root3')
+  })
+})
+
+describe('QueuedTransport', () => {
+  let transport: QueuedTransport
+
+  beforeEach(() => {
+    class QueuedTransportImpl extends QueuedTransport {}
+    transport = new QueuedTransportImpl({ local: 'local', roots: [] })
+  })
+
+  it('implements the Transport interface', () => {
+    // this will fail to compile if it doesn't implement the interface
+    const x: Transport = transport
+    expect(x).toBeDefined()
+  })
+
+  it('is an instance of ConfiguredTransport', () => {
+    expect(transport instanceof ConfiguredTransport).toBe(true)
+  })
+
+  describe('#recv', () => {
+    // @todo
+  })
+
+  describe('#stop', () => {
+    // @todo
+  })
+})
